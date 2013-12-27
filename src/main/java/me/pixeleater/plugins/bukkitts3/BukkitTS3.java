73959f28d3e4891578573a57d2a62c0bc9f04223
Brad Reardon <brad.jay.reardon@gmail.com>
2013-12-27 17:35:05 -0500

    Project skeleton, courtesy of Pluginator.

---
+++ src/main/java/me/pixeleater/plugins/bukkitts3/BukkitTS3.java
@@ -0,0 +1,22 @@
+package me.pixeleater.plugins.bukkitts3;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class BukkitTS3 extends JavaPlugin implements Listener {
+    public void onDisable() {
+        // TODO: Place any custom disable code here.
+    }
+
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        event.getPlayer().sendMessage("Welcome, " + event.getPlayer().getDisplayName() + "!");
+    }
+}
+
